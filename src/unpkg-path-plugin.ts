b100e5bd21960f535b8b7b9a110123f1c94405c0
apmfree78 <amit@profitswami.com>
2022-08-08 23:23:56 -0400

    setup dynamic calls to unpkg.com from ebuild plugin

--- src/unpkg-path-plugin.ts
+++ src/unpkg-path-plugin.ts
@@ -7,14 +7,19 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResole', args);
-        if (args.path === 'index.js') {
+        if (args.path === 'index.js')
           return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
-          return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace: 'a',
-          };
-        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+        //        } else if (args.path === 'tiny-test-pkg') {
+        //          return {
+        //            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+        //            namespace: 'a',
+        //          };
+        //        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -24,7 +29,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
