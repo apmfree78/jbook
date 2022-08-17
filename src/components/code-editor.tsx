26b24efc46023829244deac4215053cd4a656471
apmfree78 <amit@profitswami.com>
2022-08-17 16:24:10 -0400

    customized monaco editor settings

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,51 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import { useRef } from 'react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
-const CodeEditor = () => {
-  return <MonacoEditor />
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+
+  const editorRef = useRef<any>();
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+
+  }
+
+  const onFormatClick = () => {
+    // get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
+    //format that value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      semi: true,
+      singleQuote: true
+    })
+    // set the formatted value back to editor
+    editorRef.current.setValue(formatted);
+  }
+  return (
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        language='javascript'
+        options={{ wordWrap: 'on', minimap: { enabled: false }, showUnused: false, folding: false, lineNumbersMinChars: 3, fontSize: 16, scrollBeyondLastLine: false, automaticLayout: true }}
+        theme='dark' height='500px' />
+    </div>)
 };
 
 export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -66,7 +66,7 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor initialValue={'const a=1;'} onChange={(value) => setInput(value)} />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}>
