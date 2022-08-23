88dde565e587c490ecd54ae1859bbc3c9cdbb1d2
apmfree78 <amit@profitswami.com>
2022-08-22 23:04:10 -0400

    fix issues and bugs with window resizing and resize bar scrolling

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import { useState, useEffect } from 'react';
 import './resizable.css';
 
 interface ResizableProps {
@@ -7,22 +8,36 @@ interface ResizableProps {
 }
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
 
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
   let resizableProps: ResizableBoxProps;
+
+  useEffect(() => {
+    const listener = () => {
+      setInnerWidth(window.innerWidth);
+      setInnerHeight(window.innerHeight);
+    }
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    }
+  }, [])
 
   if (direction === 'horizontal') {
     resizableProps = {
       className: 'resize-horizontal',
-      maxConstraints: [window.innerWidth * 0.75, Infinity],
-      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      minConstraints: [innerWidth * 0.2, Infinity],
       resizeHandles: ['e'],
       height: Infinity,
-      width: window.innerWidth * 0.75,
+      width: innerWidth * 0.75,
     }
   }
   else {
     resizableProps = {
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       minConstraints: [Infinity, 24],
       resizeHandles: ['s'],
       height: 300,
