417db09ba69ed8745d692f5caac96023ad614146
apmfree78 <amit@profitswami.com>
2022-08-30 13:48:23 -0400

    fixed styling issues and improved styling of navigation bar

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,9 +1,41 @@
 import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css'
+import { useEffect, useState, useRef } from 'react';
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={'# Header'} />
     </div>
   );
 };
