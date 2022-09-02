a3e19f03e97df5f5d82da59a1bd389afef471103
apmfree78 <amit@profitswami.com>
2022-09-02 12:15:22 -0400

    completed action creators

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,7 +1,49 @@
-import { ActionType } from '../action-types'
-import { Action } from '../actions'
+import { ActionType } from '../action-types';
+import {
+  Action,
+  UpdateCellAction,
+  DeleteCellAction,
+  MoveCellAction,
+  InsertCellAction,
+} from '../actions';
+import { CellTypes } from '../cell';
 
-export const updateCell = () => { };
-export const deleteCell = () => { };
-export const moveCell = () => { };
-export const insertCellBefore = () => { };
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (
+  id: string,
+  direction: 'up' | 'down'
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+
+export const insertCellBefore = (id: string, type: CellTypes): InsertCellAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type
+    }
+  }
+};
