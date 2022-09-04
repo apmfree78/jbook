9bd33b472b831ea42e8a4b0531f6b292ab70c727
apmfree78 <amit@profitswami.com>
2022-09-04 14:19:01 -0400

    added immer library plus updated reducer with produce

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
@@ -18,32 +19,23 @@ const iniatialState: CellsState = {
   data: {},
 };
 
-const reducer = (
-  state: CellsState = iniatialState,
-  action: Action
-): CellsState => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-
-      return {
-        ...state,
-        data: {
-          [id]: {
-            ...state.data[id],
-            content,
-          },
-        },
-      };
-    case ActionType.DELETE_CELL:
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      return state;
-    case ActionType.MOVE_CELL:
-      return state;
-    default:
-      return state;
+const reducer = produce(
+  (state: CellsState = iniatialState, action: Action) => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        state.data[id].content = content;
+        break;
+      case ActionType.DELETE_CELL:
+        return state;
+      case ActionType.INSERT_CELL_BEFORE:
+        return state;
+      case ActionType.MOVE_CELL:
+        return state;
+      default:
+        return state;
+    }
   }
-};
+);
 
 export default reducer;
