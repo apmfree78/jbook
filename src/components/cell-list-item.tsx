d16813f00db8fa0880de483ab1fdec54da600294
apmfree78 <amit@profitswami.com>
2022-09-06 14:39:36 -0400

    connected CodeCell component to redux

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -7,7 +7,7 @@ interface CellListItemProps {
 }
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
-  if (cell.type === 'code') child = <CodeCell />;
+  if (cell.type === 'code') child = <CodeCell cell={cell} />;
   else child = <TextEditor />;
 
   return <div>{child}</div>;
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,22 +3,27 @@ import Preview from './preview';
 import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import bundle from '../bundler';
+import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
+interface CodeCellProps {
+  cell: Cell;
+}
 
-const CodeCell = () => {
-  const [input, setInput] = useState('');
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('');
   const [err, setErr] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const bundled = await bundle(input);
+      const bundled = await bundle(cell.content);
       console.log(bundled);
       setCode(bundled.code);
       setErr(bundled.err);
     }, 750);
 
     return () => clearTimeout(timer);
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <>
@@ -26,8 +31,8 @@ const CodeCell = () => {
         <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
           <Resizable direction='horizontal'>
             <CodeEditor
-              initialValue={'const a=1;'}
-              onChange={(value) => setInput(value)}
+              initialValue={cell.content}
+              onChange={(value) => updateCell(cell.id, value)}
             />
           </Resizable>
           <Preview code={code} err={err} />
