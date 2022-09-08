b3aa9ef4be095b55f64035f39c3b538f00c82255
apmfree78 <amit@profitswami.com>
2022-09-08 13:36:11 -0400

    added styling tgo add cell bar and buttons

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,7 @@
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
+import { Fragment } from 'react';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
@@ -8,10 +9,10 @@ const CellList: React.FC = () => {
   );
 
   const renderedCells = cells.map((cell) => (
-    <>
-      <AddCell key={cell.id} nextCellId={cell.id} />
-      <CellListItem key={cell.id} cell={cell} />
-    </>
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
   return (
     <div>
